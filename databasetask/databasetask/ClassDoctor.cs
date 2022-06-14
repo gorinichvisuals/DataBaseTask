a66dbe4198d594d716e2d929948107d295173a1c
Egor <gorinichstudio@gmail.com>
2022-06-14 17:02:31 +0300

    New changes and task1

---
+++ databasetask/databasetask/ClassDoctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Doctor : Person
+{
+    public string specialization;
+
+    public Doctor(string firstName, string lastName, int age, string specialization) : base(firstName, lastName, age)
+    {
+        this.specialization = specialization;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} Специализация: {specialization}";
+    }
+}
---
+++ databasetask/databasetask/ClassSoldier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Soldier : Person
+{
+    public string rank;
+
+    public Soldier(string firstName, string lastName, int age, string rank) : base(firstName, lastName, age)
+    {
+        this.rank = rank;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} Ранг военного: {rank}";
+    }
+}
